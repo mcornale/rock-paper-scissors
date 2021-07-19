334d67b61d04f8ecbcfd0f33bf8d811c9b17adc0
mcornale <massimiliano.cornale@gmail.com>
2021-07-19 09:56:58 +0200

    Add last fixes

--- src/script.js
+++ src/script.js
@@ -1,28 +1,33 @@
-import "regenerator-runtime/runtime";
+import 'regenerator-runtime/runtime';
 
-import paperImage from "/images/icon-paper.svg";
-import scissorsImage from "/images/icon-scissors.svg";
-import rockImage from "/images/icon-rock.svg";
+import paperImage from '/images/icon-paper.svg';
+import scissorsImage from '/images/icon-scissors.svg';
+import rockImage from '/images/icon-rock.svg';
 
-const GAME_ELEMENTS = ["paper", "scissors", "rock"];
+const GAME_ELEMENTS = ['paper', 'scissors', 'rock'];
 const ELEMENTS_IMAGES = [paperImage, scissorsImage, rockImage];
+const RESULTS_LABEL = {
+    WIN: 'WIN',
+    TIED: 'TIED',
+    LOSE: 'LOSE',
+};
 
-const gameChoice = document.querySelector(".game__choice");
-const gameResultsBox = document.querySelector(".game__results-box");
+const gameChoice = document.querySelector('.game__choice');
+const gameResultsBox = document.querySelector('.game__results-box');
 const playerChoiceShow = document.querySelector(
-    ".game__player .game__choice-box"
+    '.game__player .game__choice-box'
 );
 const computerChoiceShow = document.querySelector(
-    ".game__computer .game__choice-box"
+    '.game__computer .game__choice-box'
 );
-const scoreElement = document.querySelector(".score__value");
-const gameResultsText = document.querySelector(".game__result");
-const gameResultTitle = document.querySelector(".game__result-title");
-const playAgainBtn = document.querySelector(".button--play-again");
-const openPopupBtn = document.querySelector(".button--no-bg");
-const popup = document.querySelector(".popup");
-const popupBackground = document.querySelector(".popup__bg");
-const closePopupBtn = document.querySelector(".popup__close");
+const scoreElement = document.querySelector('.score__value');
+const gameResultsText = document.querySelector('.game__result');
+const gameResultTitle = document.querySelector('.game__result-title');
+const playAgainBtn = document.querySelector('.button--play-again');
+const openPopupBtn = document.querySelector('.button--no-bg');
+const popup = document.querySelector('.popup');
+const popupBackground = document.querySelector('.popup__bg');
+const closePopupBtn = document.querySelector('.popup__close');
 
 let playerChoice;
 let computerChoice;
@@ -32,21 +37,21 @@ let gameResult;
 let score = 0;
 
 const initGame = function () {
-    playerChoice = "";
-    computerChoice = "";
-    playerChoiceIndex = "";
-    computerChoiceIndex = "";
-    gameResult = "";
+    playerChoice = '';
+    computerChoice = '';
+    playerChoiceIndex = '';
+    computerChoiceIndex = '';
+    gameResult = '';
 
-    playerChoiceShow.innerHTML = "";
-    computerChoiceShow.innerHTML = "";
-    gameResultTitle.innerHTML = "";
+    playerChoiceShow.innerHTML = '';
+    computerChoiceShow.innerHTML = '';
+    gameResultTitle.innerHTML = '';
 
     transitionToInit();
 };
 
 const getPlayerChoice = function (e) {
-    const choiceEl = e.target.closest(".game__icon");
+    const choiceEl = e.target.closest('.game__icon');
 
     if (!choiceEl) return;
 
@@ -73,12 +78,13 @@ const calculateResult = function () {
             : playerChoiceIndex - 1;
 
     if (playerChoiceIndex === computerChoiceIndex) {
-        gameResult = "TIED";
+        gameResult = RESULTS_LABEL.TIED;
     } else if (playerChoiceIndexWin === computerChoiceIndex) {
-        gameResult = "WIN";
+        gameResult = RESULTS_LABEL.WIN;
         score++;
     } else {
-        gameResult = "LOSE";
+        gameResult = RESULTS_LABEL.LOSE;
+        score--;
     }
 
     transitionToSeeResults();
@@ -103,7 +109,6 @@ const generateIconMarkup = function (choice, choiceIndex) {
     return `
     <div
         class="game__icon game__icon--${choice}"
-        data-choice="paper"
     >
         <img src="${ELEMENTS_IMAGES[choiceIndex]}" alt="Paper icon" />
     </div>
@@ -111,135 +116,153 @@ const generateIconMarkup = function (choice, choiceIndex) {
 };
 
 const openPopup = async function () {
-    popup.classList.remove("hidden");
-    popupBackground.classList.remove("hidden");
+    popup.classList.remove('hidden');
+    popupBackground.classList.remove('hidden');
 
-    popupBackground.animate([{ opacity: "0" }, { opacity: "1" }], {
+    popupBackground.animate([{ opacity: '0' }, { opacity: '1' }], {
         duration: 200,
-        easing: "ease-in",
-        fill: "both",
+        easing: 'ease-in',
+        fill: 'both',
     });
 
     popup.animate(
         [
-            { transform: "translate(-50%, -50%) scale(0.8)", opacity: "0" },
-            { transform: "translate(-50%, -50%) scale(1)", opacity: "1" },
+            {
+                transform: 'translate(-50%, -50%) scale(0.8)',
+                opacity: '0',
+            },
+            { transform: 'translate(-50%, -50%) scale(1)', opacity: '1' },
         ],
         {
             duration: 400,
-            easing: "cubic-bezier(0.42, 0, 0, 0.92)",
-            fill: "both",
+            easing: 'cubic-bezier(0.42, 0, 0, 0.92)',
+            fill: 'both',
         }
     );
 };
 
 const closePopup = async function () {
-    popupBackground.animate([{ opacity: "1" }, { opacity: "0" }], {
+    popupBackground.animate([{ opacity: '1' }, { opacity: '0' }], {
         duration: 200,
-        easing: "ease-in",
-        fill: "both",
+        easing: 'ease-in',
+        fill: 'both',
     });
 
     await popup.animate(
         [
-            { transform: "translate(-50%, -50%) scale(1)" },
-            { transform: "translate(-50%, -50%) scale(0)" },
+            { transform: 'translate(-50%, -50%) scale(1)' },
+            {
+                transform: 'translate(-50%, -50%) scale(0)',
+                borderRadius: '50%',
+            },
         ],
         {
             duration: 400,
-            easing: "cubic-bezier(0.42, 0, 0, 0.92)",
-            fill: "both",
+            easing: 'cubic-bezier(0.42, 0, 0, 0.92)',
+            fill: 'both',
         }
     ).finished;
 
-    popup.classList.add("hidden");
-    popupBackground.classList.add("hidden");
+    popup.animate(
+        [
+            {
+                borderRadius: '1rem',
+            },
+        ],
+        {
+            duration: 0,
+            fill: 'both',
+        }
+    );
+
+    popup.classList.add('hidden');
+    popupBackground.classList.add('hidden');
 };
 
 const transitionToSeeResults = async function () {
     const iconScale = getComputedStyle(
         document.documentElement
-    ).getPropertyValue("--scale-icon-value");
+    ).getPropertyValue('--scale-icon-value');
 
     await gameChoice.animate(
-        [{ transform: "scale(1)" }, { transform: "scale(0.4)", opacity: "0" }],
+        [{ transform: 'scale(1)' }, { transform: 'scale(0.4)', opacity: '0' }],
         {
             duration: 600,
-            easing: "cubic-bezier(0.42, 0, 0, 0.92)",
-            fill: "both",
+            easing: 'cubic-bezier(0.42, 0, 0, 0.92)',
+            fill: 'both',
         }
     ).finished;
 
-    gameResultsBox.classList.remove("hidden");
+    gameResultsBox.classList.remove('hidden');
 
     await gameResultsBox.animate(
         [
-            { transform: "scale(0.4)", opacity: "0" },
-            { transform: "scale(1)", opacity: "1" },
+            { transform: 'scale(0.4)', opacity: '0' },
+            { transform: 'scale(1)', opacity: '1' },
         ],
         {
             duration: 300,
-            easing: "linear",
-            fill: "both",
+            easing: 'linear',
+            fill: 'both',
         }
     ).finished;
 
-    await playerChoiceShow.querySelector(".game__icon").animate(
+    await playerChoiceShow.querySelector('.game__icon').animate(
         [
-            { transform: "scale(0.4)", opacity: "0" },
-            { transform: `scale(${iconScale})`, opacity: "1" },
+            { transform: 'scale(0.4)', opacity: '0' },
+            { transform: `scale(${iconScale})`, opacity: '1' },
         ],
         {
             duration: 600,
-            easing: "cubic-bezier(0.42, 0, 0, 0.92)",
-            fill: "both",
+            easing: 'cubic-bezier(0.42, 0, 0, 0.92)',
+            fill: 'both',
         }
     ).finished;
 
-    await computerChoiceShow.querySelector(".game__icon").animate(
+    await computerChoiceShow.querySelector('.game__icon').animate(
         [
-            { transform: "scale(0.4)", opacity: "0" },
-            { transform: `scale(${iconScale})`, opacity: "1" },
+            { transform: 'scale(0.4)', opacity: '0' },
+            { transform: `scale(${iconScale})`, opacity: '1' },
         ],
         {
             duration: 600,
-            easing: "cubic-bezier(0.42, 0, 0, 0.92)",
-            fill: "both",
+            easing: 'cubic-bezier(0.42, 0, 0, 0.92)',
+            fill: 'both',
         }
     ).finished;
 
-    if (getComputedStyle(gameResultsText).order !== "3") {
+    if (getComputedStyle(gameResultsText).order !== '3') {
         await gameResultsText.animate(
             [
-                { transform: "scale(0)", width: "0rem" },
-                { transform: "scale(1)", width: "30rem" },
+                { transform: 'scale(0)', width: '0rem' },
+                { transform: 'scale(1)', width: '30rem' },
             ],
             {
                 duration: 600,
-                easing: "ease-in-out",
-                fill: "both",
+                easing: 'ease-in-out',
+                fill: 'both',
             }
         ).finished;
     }
 
-    gameResultTitle.classList.remove("hidden");
-    playAgainBtn.classList.remove("hidden");
+    gameResultTitle.classList.remove('hidden');
+    playAgainBtn.classList.remove('hidden');
 
     gameResultTitle.animate(
-        [{ transform: "scale(0)" }, { transform: `scale(1)` }],
+        [{ transform: 'scale(0)' }, { transform: `scale(1)` }],
         {
             duration: 600,
-            easing: "cubic-bezier(0.42, 0, 0, 0.92)",
-            fill: "both",
+            easing: 'cubic-bezier(0.42, 0, 0, 0.92)',
+            fill: 'both',
         }
     ).finished;
 
     playAgainBtn.animate(
-        [{ transform: "scale(0)" }, { transform: `scale(1)` }],
+        [{ transform: 'scale(0)' }, { transform: `scale(1)` }],
         {
             duration: 600,
-            easing: "cubic-bezier(0.42, 0, 0, 0.92)",
-            fill: "both",
+            easing: 'cubic-bezier(0.42, 0, 0, 0.92)',
+            fill: 'both',
         }
     ).finished;
 
@@ -249,46 +272,46 @@ const transitionToSeeResults = async function () {
 const transitionToInit = async function () {
     await gameResultsBox.animate(
         [
-            { transform: "scale(1)", opacity: "1" },
-            { transform: "scale(0.4)", opacity: "0" },
+            { transform: 'scale(1)', opacity: '1' },
+            { transform: 'scale(0.4)', opacity: '0' },
         ],
         {
             duration: 600,
-            easing: "cubic-bezier(0.42, 0, 0, 0.92)",
-            fill: "both",
+            easing: 'cubic-bezier(0.42, 0, 0, 0.92)',
+            fill: 'both',
         }
     ).finished;
 
-    gameResultsBox.classList.add("hidden");
+    gameResultsBox.classList.add('hidden');
 
     await gameChoice.animate(
-        [{ transform: "scale(0.4)" }, { transform: "scale(1)", opacity: "1" }],
+        [{ transform: 'scale(0.4)' }, { transform: 'scale(1)', opacity: '1' }],
         {
             duration: 300,
-            easing: "linear",
-            fill: "both",
+            easing: 'linear',
+            fill: 'both',
         }
     ).finished;
 
-    gameResultTitle.classList.add("hidden");
-    playAgainBtn.classList.add("hidden");
+    gameResultTitle.classList.add('hidden');
+    playAgainBtn.classList.add('hidden');
 
-    if (getComputedStyle(gameResultsText).order !== "3") {
+    if (getComputedStyle(gameResultsText).order !== '3') {
         gameResultsText.animate(
             [
-                { transform: "scale(1)", width: "30rem" },
-                { transform: "scale(0)", width: "0rem" },
+                { transform: 'scale(1)', width: '30rem' },
+                { transform: 'scale(0)', width: '0rem' },
             ],
             {
                 duration: 0,
-                easing: "ease-in-out",
-                fill: "both",
+                easing: 'ease-in-out',
+                fill: 'both',
             }
         );
     }
 };
 
-gameChoice.addEventListener("click", getPlayerChoice);
-playAgainBtn.addEventListener("click", initGame);
-openPopupBtn.addEventListener("click", openPopup);
-closePopupBtn.addEventListener("click", closePopup);
+gameChoice.addEventListener('click', getPlayerChoice);
+playAgainBtn.addEventListener('click', initGame);
+openPopupBtn.addEventListener('click', openPopup);
+closePopupBtn.addEventListener('click', closePopup);
